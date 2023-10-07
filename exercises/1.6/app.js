c5e00b4933df467ccaca92c51129492f295531d9
didia <didia.josefechado@student.vinci.be>
2023-10-07 13:02:15 +0200

    1.6 : API : delete & modify

--- exercises/1.6/app.js
+++ exercises/1.6/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var filmRouter=require('./routes/films');
 
 var app = express();
 
@@ -14,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/films', filmRouter);
+
 
 module.exports = app;
